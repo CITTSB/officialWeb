f45932eb1debc6a79341e20f0b27a9048ba1a3fe
Sebastian Orrego <s.orregoa@alumnos.duoc.cl>
2018-07-09 15:53:56 -0400

    formulario try catch

--- PaginaWeb/formulario.php
+++ PaginaWeb/formulario.php
@@ -1,91 +1,91 @@
 <?php
 
+use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\POP3;
-use PHPMailer\PHPMailer\Exception;
 
 require 'PHPMailer/PHPMailer.php';
 require 'PHPMailer/POP3.php';
 require 'PHPMailer/SMTP.php';
 require 'PHPMailer/Exception.php';
 
-if (isset($_POST["opcion"])){
-    if($_POST["opcion"]=="Enviar Mensaje"){
+if (isset($_POST["opcion"])) {
+    if ($_POST["opcion"] == "Enviar Mensaje") {
         //Envio de correo recepcion
         $mail = new PHPMailer(true);
+        try {
+            $correo = $_POST['correo'];
+            $mensaje = $_POST['mensaje'];
+            $nombre = $_POST['nombre'];
 
-        $correo = $_POST['correo'];
-        $mensaje = $_POST['mensaje'];
-        $nombre = $_POST['nombre'];
+            $mail->isSMTP(); // Set mailer to use SMTP xd
+            $mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers
+            $mail->SMTPAuth = true; // Enable SMTP authentication
+            $mail->Username = 'cittsb.contacto@gmail.com'; // SMTP username
+            $mail->Password = 'abcd13abcd'; // SMTP password
+            $mail->SMTPSecure = 'ssl';
+            $mail->IsHTML(true); // Enable TLS encryption, `ssl` also accepted
+            $mail->Port = 465; // TCP port to connect to
 
-        $mail->isSMTP();                                      // Set mailer to use SMTP xd
-        $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-        $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail->Username = 'cittsb.contacto@gmail.com';                 // SMTP username
-        $mail->Password = 'abcd13abcd';                           // SMTP password
-        $mail->SMTPSecure = 'ssl';       
-        $mail->IsHTML(true);                     // Enable TLS encryption, `ssl` also accepted
-        $mail->Port = 465;                                    // TCP port to connect to
+            $mail->setFrom('cittsb.contacto@gmail.com', 'Contacto Capitanes CITT San Bernardo');
+            // $mail->addAddress($correo, $nombre);
 
-        $mail->setFrom('cittsb.contacto@gmail.com', 'Contacto Capitanes CITT San Bernardo');
-       // $mail->addAddress($correo, $nombre);     
-       
-       $mail->addAddress('by.sepulvedas@alumnos.duoc.cl');            
-       $mail->addAddress('leon.romero@alumnos.duoc.cl');     
-        $mail->addAddress('b.morat@alumnos.duoc.cl');     
-        $mail->Subject = 'Nuevo correo de ' . $correo;
-        $m = "<strong>Nombre: " . $nombre .
-             "</strong> <br> <br> <strong> correo:</strong>" . $correo .
-              " <br> <br>  <strong>mensaje:</strong> " . $mensaje;
-        $mail->Body = $m;
+            $mail->addAddress('by.sepulvedas@alumnos.duoc.cl');
+            $mail->addAddress('leon.romero@alumnos.duoc.cl');
+            $mail->addAddress('b.morat@alumnos.duoc.cl');
+            $mail->Subject = 'Nuevo correo de ' . $correo;
+            $m = "<strong>Nombre: " . $nombre .
+                "</strong> <br> <br> <strong> correo:</strong>" . $correo .
+                " <br> <br>  <strong>mensaje:</strong> " . $mensaje;
+            $mail->Body = $m;
+        } catch (Exception $e) {
+            echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+        }
 
         //correo al que lo envio
         $mail2 = new PHPMailer(true);
-        $mail2->isSMTP();                                      // Set mailer to use SMTP
-        $mail2->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-        $mail2->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail2->Username = 'cittsb.contacto@gmail.com';                 // SMTP username
-        $mail2->Password = 'abcd13abcd';                           // SMTP password
-        $mail2->SMTPSecure = 'ssl';       
-        $mail2->IsHTML(true);                     // Enable TLS encryption, `ssl` also accepted
-        $mail2->Port = 465;                                    // TCP port to connect to
+        try {
+            $mail2->isSMTP(); // Set mailer to use SMTP
+            $mail2->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers
+            $mail2->SMTPAuth = true; // Enable SMTP authentication
+            $mail2->Username = 'cittsb.contacto@gmail.com'; // SMTP username
+            $mail2->Password = 'abcd13abcd'; // SMTP password
+            $mail2->SMTPSecure = 'ssl';
+            $mail2->IsHTML(true); // Enable TLS encryption, `ssl` also accepted
+            $mail2->Port = 465; // TCP port to connect to
 
-        $mail2->setFrom('cittsb.contacto@gmail.com', 'Contacto Capitanes CITT San Bernardo');
-        $mail2->addAddress($correo);     
-        $mail2->Subject = 'Hemos recibido su mensaje';
-        $m = "Le informamos que CITTSB a recibido su mensaje. <br> Su mensaje será respondido dentro de las 24 horas. ";
-        $mail2->Body = $m;
+            $mail2->setFrom('cittsb.contacto@gmail.com', 'Contacto Capitanes CITT San Bernardo');
+            $mail2->addAddress($correo);
+            $mail2->Subject = 'Hemos recibido su mensaje';
+            $m = "Le informamos que CITTSB a recibido su mensaje. <br> Su mensaje será respondido dentro de las 24 horas. ";
+            $mail2->Body = $m;
 
-        if($mail2->Send())
-        {
+            if ($mail2->Send()) {
 
-            if($mail->Send())
-            {
-                echo'<script type="text/javascript">
+                if ($mail->Send()) {
+                    echo '<script type="text/javascript">
                         alert("Su mensaje ha sido enviado");
                         window.location="index.html"
                    </script>';
-            }else{
-                echo'<script type="text/javascript">
+                } else {
+                    echo '<script type="text/javascript">
                 alert("NO ENVIADO, intentar de nuevo");
                 window.location="index.html"
                 </script>';
-            }
-        }
-        else{
-            echo'<script type="text/javascript">
+                }
+            } else {
+                echo '<script type="text/javascript">
                     alert("NO ENVIADO, intentar de nuevo");
                     window.location="index.html"
                     </script>';
+            }
+        } catch (Exception $e) {
+            echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
         }
     }
-}else{
-    echo'<script type="text/javascript">
+} else {
+    echo '<script type="text/javascript">
     alert("error 404 . Pagina no encontrada");
     window.location="index.html"
     </script>';
 }
-
-
-?>
